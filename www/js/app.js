81addca1994719ef12103463f2bfa730d8f3b0e9
David East <david@firebase.com>
2015-03-04 05:45:43 -0800

    Make it so.

---
+++ www/js/app.js
@@ -0,0 +1,40 @@
+
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+  .state('tab', {
+    url: "/tab",
+    abstract: true,
+    templateUrl: "templates/tabs.html"
+  })
+
+  .state('tab.contacts', {
+    url: '/contacts',
+    views: {
+      'tab-contacts': {
+        templateUrl: 'templates/tab-contacts.html',
+        controller: 'ContactsCtrl'
+      }
+    }
+  });
+
+  $urlRouterProvider.otherwise('/tab/contacts');
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,16 @@
+angular.module('starter.controllers', ['firebase'])
+
+.controller('ContactsCtrl', function($scope, $firebaseArray, People) {
+
+  var baseRef = new Firebase('https://webapi.firebaseio.com/rolodex');
+  var scrollRef = new Firebase.util.Scroll(baseRef, 'name');
+
+  $scope.items = $firebaseArray(scrollRef);
+  scrollRef.scroll.next(3);
+
+  $scope.loadMore = function() {
+    scrollRef.scroll.next(1);
+    $scope.$broadcast('scroll.infiniteScrollComplete');
+  };
+
+});
